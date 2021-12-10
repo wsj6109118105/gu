77148af5fe4ed9e98d265594b78a7c1c8ca1f8f4
happy <2362487738@qq.com>
2021-12-10 18:29:27 +0800

    接入rabbitmq，开始订单服务

--- order/src/main/java/com/order/OrderApplication.java
+++ order/src/main/java/com/order/OrderApplication.java
@@ -1,8 +1,16 @@
 package com.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 引入 rabbitMQ
+ */
+
+@EnableRabbit
+@EnableFeignClients
 @SpringBootApplication
 public class OrderApplication {
 
--- order/src/test/java/com/order/OrderApplicationTests.java
+++ order/src/test/java/com/order/OrderApplicationTests.java
@@ -1,13 +1,41 @@
 package com.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class OrderApplicationTests {
 
+    /**
+     * 1.如何创建 exchange , queue , 绑定关系
+     *      1),使用 amqpAdmin 进行创建
+     */
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
     @Test
-    void contextLoads() {
+    void createExchange() {
+        amqpAdmin.declareExchange(new DirectExchange("hello.java.exchange"));
+        log.info("交换机打印完成");
+    }
+
+    @Test
+    void createQueue() {
+        amqpAdmin.declareQueue(new Queue("hello.java.queue"));
+        log.info("队列创建完成");
+    }
+
+    @Test
+    void binding() {
+        amqpAdmin.declareBinding(new Binding("hello.java.queue", Binding.DestinationType.QUEUE,"hello.java.exchange","hello.java.queue",null));
+        log.info("关系绑定完成");
     }
 
 }
